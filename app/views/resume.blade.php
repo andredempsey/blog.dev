ed0e42a694a6af7b1c0dbfe829a99fe59061df3b
Andre Dempsey <andre.dempsey@live.com>
2014-07-01 08:24:09 -0500

    modified resume content; added timeline to bottom of page

--- app/views/resume.blade.php
+++ app/views/resume.blade.php
@@ -2,8 +2,6 @@
 
 @section('content')
 
-
-
 <h3 id="topParagraph">Summary:</h3>
 <p>Experienced professional consultant, database developer, data analyst, and veteran Air Force officer  
 <span>with 20+ years’ experience successfully leading people in program, project, and facility management activities</span>
@@ -12,7 +10,7 @@
 
 
 <h3>Experience:</h3>
-<span class='companyName'>MWH Global, <span class='location'>San Antonio, TX</span> </span><span class='dates'>Jun 2012 – Apr 2013</span><br>
+<span class='companyName'>MWH Global, <span class='location'>San Antonio, TX</span> </span><span class='dates'>Jun 2012 – Apr 2014</span><br>
 <span class='jobTitle'>Infrastructure Data Management Specialist</span> 
 <p class='accomplishment'>
 	<ul class="accomplishments">
@@ -23,7 +21,7 @@
 	</ul>
 </p>
       
-<span class='companyName'>Booz Allen Hamilton (Client:  HQ 502 Air Base Wing)<span class='location'>San Antonio, TX</span> </span><span class='dates'>Sep 2010 to May 2012</span><br>
+<span class='companyName'>Booz Allen Hamilton (Client:  HQ 502 Air Base Wing) <span class='location'>San Antonio, TX</span> </span><span class='dates' id='2010'>Sep 2010 to May 2012</span><br>
 <span class='jobTitle'>Associate (Management Consulting)</span>
 <p class='accomplishment'>
 	<ul class="accomplishments">
@@ -32,16 +30,8 @@
 		<li class="bulletAccomplish">Analyzed migrated data from the Army’s Integrated Facilities System (IFS) database to the Air Forces ACES real property database.  Identified anomalies and coordinated with Air Force database experts to implement corrections.</li>
 	</ul>
 </p>
-<span class='companyName'>HQ Air Education Training Command<span class='location'>Randolph AFB, TX</span> </span><span class='dates'>Jun 2008 to Jul 2010</span><br>
-<span class='jobTitle'>Operations Support Section Chief</span> 
-<p class='accomplishment'>
-	<ul class="accomplishments">
-		<li class="bulletAccomplish">Led 9 program managers in providing policy and oversight to civil engineer organizations at 12 Air Force bases with a total physical plant value of $17.5B.</li>
-		<li class="bulletAccomplish">Brought in as lead project manager responsible for planning, organizing, and establishing the civil engineering organization required to support the Department of Defense’s largest Joint Base; combined facility support functions from Lackland AFB, Randolph AFB, and Ft Sam Houston. Developed project management plan, budget plan, personnel training plan, and memorandum of agreement.
-	</ul>
-</p>
 
-<span class='companyName'><span class='location'>JB Balad, Iraq</span> </span><span class='dates'>Oct 2009 to Apr 2010</span><br>
+<span class='companyName'><span class='location'>JB Balad, Iraq</span> </span><span class='dates' id='2009'>Oct 2009 to Apr 2010</span><br>
 <span class='jobTitle'>Facilities Engineer Team Chief</span> 
 <p class='accomplishment'>
 	<ul class="accomplishments">
@@ -52,7 +42,26 @@ and other repairs worth $42M in essential infrastructure for the people of Iraq.
 	</ul>
 </p>
 
-<span class='companyName'>HQ United States Air Forces Europe<span class='location'>Ramstein AB, GE</span> </span><span class='dates'>Jun 2005 to Jun 2008</span><br>
+<span class='companyName'>HQ Air Education Training Command<span class='location'>Randolph AFB, TX</span> </span><span class='dates' id='2008'>Jun 2008 to Jul 2010</span><br>
+<span class='jobTitle'>Operations Support Section Chief</span> 
+<p class='accomplishment'>
+	<ul class="accomplishments">
+		<li class="bulletAccomplish">Led 9 program managers in providing policy and oversight to civil engineer organizations at 12 Air Force bases with a total physical plant value of $17.5B.</li>
+		<li class="bulletAccomplish">Brought in as lead project manager responsible for planning, organizing, and establishing the civil engineering organization required to support the Department of Defense’s largest Joint Base; combined facility support functions from Lackland AFB, Randolph AFB, and Ft Sam Houston. Developed project management plan, budget plan, personnel training plan, and memorandum of agreement.
+	</ul>
+</p>
+
+<span class='companyName'>USACE Gulf Region Division<span class='location'>Baghdad, Iraq </span> </span><span class='dates' id='2007'>Jun 2007 to Dec 2007</span><br>
+<span class='jobTitle'>Program Manager</span> 
+<p class='accomplishment'>
+	<ul class="accomplishments">
+		<li class="bulletAccomplish">Oversaw 7 program managers in the management and execution of a $295M Iraqi Reconstruction Relief Fund construction program supporting Transportation and Communications projects throughout Iraq.</li>
+		<li class="bulletAccomplish">Managed a $220M non-construction program that provided equipment and operations & maintenance services. </li>
+		<li class="bulletAccomplish">Coordinated with Iraqi ministry officials to restart the $68M Iraqi roads and bridges Project Partnership Agreement.</li>
+	</ul>
+</p>
+
+<span class='companyName'>HQ United States Air Forces Europe<span class='location'>Ramstein AB, GE</span> </span><span class='dates' id='2005'>Jun 2005 to Jun 2008</span><br>
 <span class='jobTitle'>Chief, Housing Branch</span> 
 <p class='accomplishment'>
 	<ul class="accomplishments">
@@ -65,17 +74,7 @@ and other repairs worth $42M in essential infrastructure for the people of Iraq.
 	</ul>
 </p>
 
-<span class='companyName'>USACE Gulf Region Division<span class='location'>Baghdad, Iraq </span> </span><span class='dates'>Jun 2007 to Dec 2007</span><br>
-<span class='jobTitle'>Program Manager</span> 
-<p class='accomplishment'>
-	<ul class="accomplishments">
-		<li class="bulletAccomplish">Oversaw 7 program managers in the management and execution of a $295M Iraqi Reconstruction Relief Fund construction program supporting Transportation and Communications projects throughout Iraq.</li>
-		<li class="bulletAccomplish">Managed a $220M non-construction program that provided equipment and operations & maintenance services. </li>
-		<li class="bulletAccomplish">Coordinated with Iraqi ministry officials to restart the $68M Iraqi roads and bridges Project Partnership Agreement.</li>
-	</ul>
-</p>
-
-<span class='companyName'>AF Civil Engineer & Services School<span class='location'>Wright Patterson AFB, OH</span> </span><span class='dates'>Mar 03 to Jun 05</span><br>
+<span class='companyName'>AF Civil Engineer & Services School<span class='location'>Wright Patterson AFB, OH</span> </span><span class='dates' id='2003'>Mar 2003 to Jun 2005</span><br>
 <span class='jobTitle'>Director, Readiness Management Dept</span> 
 <p class='accomplishment'>
 	<ul class="accomplishments">
@@ -95,5 +94,19 @@ an overhaul of the initial wartime engineering skills course which yielded an im
 	<li>Squadron Officers School (Entry-Level Military Leadership Course)</li>
 </ul>
 
+<div class="navbar navbar-fixed-bottom navbar-inverse">
+	<span class="col-xs-1 timeline-odd"><a href="#2004">2003</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#2004">2004</a></span>
+	<span class="col-xs-1 timeline-odd"><a href="#2006">2005</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#2006">2006</a></span>
+	<span class="col-xs-1 timeline-odd"><a href="#2008">2007</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#2006">2008</a></span>
+	<span class="col-xs-1 timeline-odd"><a href="#2010">2009</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#2011">2010</a></span>
+	<span class="col-xs-1 timeline-odd"><a href="#2011">2011</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#2011">2012</a></span>
+	<span class="col-xs-1 timeline-odd"><a href="#topParagraph">2013</a></span>
+	<span class="col-xs-1 timeline-even"><a href="#topParagraph">2014</a></span>
+</div>
 
 @stop
