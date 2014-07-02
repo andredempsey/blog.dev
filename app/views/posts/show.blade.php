0564be67f90ecf90ee4e3c02a2859ce9edb7abc3
Andre Dempsey <andre.dempsey@live.com>
2014-07-02 09:08:31 -0500

    reformatted show blog view and added back button

--- app/views/posts/show.blade.php
+++ app/views/posts/show.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 
 <div class="container" id="topParagraph">
-	<h3>{{{ $post->title }}}</h3>
-	<h5>{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}}}</h5>
-	
+	<button type="button" class="btn btn-default pull-right">{{link_to_action('PostsController@index', 'back')}}</button>
+	<h2>{{{ $post->title }}}</h2>
+	<h6>{{{ $post->created_at->setTimezone('America/Chicago')->format('l, F jS Y @ h:i:s A')}}}</h6>
+	<hr>
 	<p>{{{ $post->body }}}</p>
 
 </div>
