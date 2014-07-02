4447e3edaec80ca61b1e5c1bed1eb3c39b3c545f
Andre Dempsey <andre.dempsey@live.com>
2014-07-02 14:37:10 -0500

    added seed method for posts table

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -12,6 +12,7 @@ class DatabaseSeeder extends Seeder {
 		Eloquent::unguard();
 
 		$this->call('UserTableSeeder');
+		$this->call('PostTableSeeder');
 	}
 
 }
@@ -26,6 +27,23 @@ class UserTableSeeder extends Seeder {
         $user->email = 'admin@codeup.com';
         $user->password = Hash::make('adminPass123!');
         $user->save();
+    }
+
+}
+
+class PostTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('posts')->delete();
+        for ($i=1;$i<=10;$i++ ) 
+        {
+	        $post = new Post();
+	        $post->title = "Blog #$i";
+	        $post->body = "body text for post $i";
+	        $post->save();
+	        sleep(1);
+        }
     }
 
 }
