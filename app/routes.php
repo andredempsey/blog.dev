12b33782959fe98eabc8ec843836e5e2cc7dbfee
Andre Dempsey <andre.dempsey@live.com>
2014-07-26 10:18:53 -0500

    updated profile picture and route to scale picture

--- app/routes.php
+++ app/routes.php
@@ -36,5 +36,57 @@ Route::get('/terms-privacy', 'HomeController@showTermsPrivacy');
 
 Route::get('/admin', 'HomeController@showAdmin');
 
+Route::get('test', function () {
 
+    $maxHeight = 200;
+    $maxWidth = 200;
+
+    $newHeight = 0;
+    $newWidth = 0;
+
+    $inputFile = public_path() . '/uploads/ct.jpg';
+    $outputFile = public_path() . '/uploads/ct-small.jpg';
+
+    // load the image to be manipulated
+    $image = new Imagick($inputFile);
+
+    // get the current image dimensions
+    $currentWidth = $image->getImageWidth(); 
+    $currentHeight = $image->getImageHeight();
+
+    // determine what the new height and width should be based on the type of photo
+    if ($currentWidth > $currentHeight)
+    {
+        // landscape photo
+        // width should be resized to max and height should be resized proportionally
+        $newWidth = $maxWidth;
+        $newHeight = ceil($currentHeight * ($newWidth / $currentWidth));
+    }
+    else if ($currentHeight > $currentWidth)
+    {
+        // portrait photo
+        // height should be resized to max and width should be resized proportionally
+        $newHeight = $maxHeight;
+        $newWidth = ceil($currentWidth * ($newHeight / $currentHeight));
+    }
+    else
+    {
+        // square photo
+        // resize image to max dimensions
+        $newHeight = $newWidth = $maxHeight;
+    }
+
+    // perform the image resize
+    $image->resizeImage($newWidth, $newHeight, Imagick::FILTER_LANCZOS, true);  
+
+    // write out the new image
+    $image->writeImage($outputFile);
+
+    // clear memory resources
+    $image->clear(); 
+    $image->destroy();
+
+    return 'Done';
+
+});
 
